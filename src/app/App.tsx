bdf451830eb51385b2b2a086265f5b9460f90143
breznovic <slymarkel@gmail.com>
2022-03-13 20:56:48 +0300

    Thunk 3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './App.css'
 import { TodolistsList } from '../features/TodolistsList/TodolistsList'
 
+// You can learn about the difference by reading this guide on minimizing bundle size.
+// https://mui.com/guides/minimizing-bundle-size/
+// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
