524c1783540e393a30480a63685411f0c054d119
breznovic <slymarkel@gmail.com>
2022-08-26 13:39:41 +0300

    Start App again

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React from 'react'
-import './App.css'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import { Menu } from '@mui/icons-material';
-import {TodolistsList} from "./features/TodolistsList/TodolistsList";
-
+import s from './App.module.css'
 
 function App() {
 
     return (
-        <div className="App">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Todolist
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-            <Container fixed>
-                <TodolistsList/>
-            </Container>
+        <div>
+          Hello
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './app/App'
-import { store } from './app/store'
-import {Provider} from "react-redux";
+import s from './index.module.css'
+import App from './App'
 
 ReactDOM.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>,
+            <App/>,
     document.getElementById('root')
 )
 
