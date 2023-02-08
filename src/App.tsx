426e06051955fc0dac31c53520cac301b31b3a71
breznovic <slymarkel@gmail.com>
2023-02-08 19:18:08 +0300

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,7 @@
-import React, {useCallback} from 'react'
-import s from './App.module.css'
-import {Todolist} from "./components/Todolist"
-import {AppInput} from "./components/common/AppInput/AppInput"
-import {useDispatch, useSelector} from "react-redux"
-import {
-    addTodolistAC,
-    changeTodolistFilterAC,
-    changeTodolistTitleAC,
-    removeTodolistAC
-} from "./state/reducers/todolistsReducer"
-import {AppRootState} from "./state/store"
-
-export type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
-
-export type TodolistType = {
-    id: string
-    title: string
-    filter: FilterType
-}
-
-export type TaskStateType = { [key: string]: TaskType[] }
-
-export type FilterType = 'all' | 'active' | 'completed'
+import s from "./App.module.css";
 
 function App() {
-
-    const dispatch = useDispatch()
-    const todolists = useSelector<AppRootState, TodolistType[]>((state) => state.todolists)
-
-    const removeTodolist = useCallback((id: string) => {
-        dispatch(removeTodolistAC(id))
-    }, [dispatch])
-
-    const changeTodolistTitle = useCallback((id: string, newTitle: string) => {
-        dispatch(changeTodolistTitleAC(id, newTitle))
-    }, [dispatch])
-
-    const addTodolist = useCallback((title: string) => {
-        dispatch(addTodolistAC(title))
-    }, [dispatch])
-
-    const changeFilter = useCallback((value: FilterType, todolistId: string) => {
-        dispatch(changeTodolistFilterAC(todolistId, value))
-    }, [dispatch])
-
-    return <div className={s.img}>
-        <div className={s.element}>
-            <div className={s.input}><AppInput addItem={addTodolist}/></div>
-            {
-                todolists.map(tl => {
-
-                        return <div className={s.todo}>
-                            <Todolist key={tl.id}
-                                      name={tl.title}
-                                      changeFilter={changeFilter}
-                                      filter={tl.filter}
-                                      id={tl.id}
-                                      removeTodolist={removeTodolist}
-                                      changeTodolistTitle={changeTodolistTitle}
-                            />
-                        </div>
-                    }
-                )
-            }
-        </div>
-    </div>
+  return <div>Test</div>;
 }
 
-export default App
+export default App;
