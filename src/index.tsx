76d2bd18ef3c518600471f73ccf5f049328d43cd
breznovic <slymarkel@gmail.com>
2023-09-01 18:47:26 +0300

    Reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app/App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
 );
+
