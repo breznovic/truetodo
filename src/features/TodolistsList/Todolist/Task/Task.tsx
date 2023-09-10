6f61cd854266edb2cd01d20831ab262239ea31ba
breznovic <slymarkel@gmail.com>
2023-09-10 19:19:58 +0300

    Without slider

--- src/features/TodolistsList/Todolist/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Task/Task.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { IconButton } from "@mui/material";
 import { EditableSpan } from "../../../../components/EditableSpan/EditableSpan";
 import { Delete } from "@mui/icons-material";
 import { TaskStatuses, TaskType } from "../../../../api/todolists-api";
-import { Checkbox, Slider } from "antd";
+import { Checkbox } from "antd";
 import { CheckboxChangeEvent } from "antd/es/checkbox";
 
 type TaskPropsType = {
@@ -46,8 +46,6 @@ export const Task = React.memo((props: TaskPropsType) => {
     [props.task.id, props.todolistId]
   );
 
-  const [disabled, setDisabled] = useState(false);
-
   return (
     <div
       key={props.task.id}
@@ -62,8 +60,6 @@ export const Task = React.memo((props: TaskPropsType) => {
       <IconButton onClick={onClickHandler}>
         <Delete />
       </IconButton>
-      <Slider defaultValue={30} disabled={disabled} />
-      <Slider range defaultValue={[20, 50]} disabled={disabled} />
     </div>
   );
 });
