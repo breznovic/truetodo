c8de06efab43672e690761871f5259164cb25820
breznovic <slymarkel@gmail.com>
2023-10-05 17:32:50 +0300

    Form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -112,6 +112,20 @@ function App({ demo = false }: PropsType) {
         </ThemeProvider>
       </div>
     </BrowserRouter>
+    <FormControl fullWidth>
+    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      value={age}
+      label="Age"
+      onChange={handleChange}
+    >
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>
   );
 }
 
