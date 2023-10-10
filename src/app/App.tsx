0ed04587f72545f3aa6750a8fb855801a5a0ec76
breznovic <slymarkel@gmail.com>
2023-10-10 22:18:50 +0300

    Switch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -112,20 +112,11 @@ function App({ demo = false }: PropsType) {
         </ThemeProvider>
       </div>
     </BrowserRouter>
-    <FormControl fullWidth>
-    <InputLabel id="demo-simple-select-label">Age</InputLabel>
-    <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      value={age}
-      label="Age"
-      onChange={handleChange}
-    >
-      <MenuItem value={10}>Ten</MenuItem>
-      <MenuItem value={20}>Twenty</MenuItem>
-      <MenuItem value={30}>Thirty</MenuItem>
-    </Select>
-  </FormControl>
+   <FormGroup>
+   <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+   <FormControlLabel required control={<Switch />} label="Required" />
+   <FormControlLabel disabled control={<Switch />} label="Disabled" />
+ </FormGroup>
   );
 }
 
