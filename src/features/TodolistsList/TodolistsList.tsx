5b6a50025a66537700351b4eb4bbde7f07aab70e
breznovic <slymarkel@gmail.com>
2023-12-11 19:39:48 +0300

    Skeleton draft

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -1,8 +1,12 @@
 import React, { useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { FilterValuesType, todolistsActions, todolistsThunks } from "features/TodolistsList/todolists.reducer";
+import {
+  FilterValuesType,
+  todolistsActions,
+  todolistsThunks,
+} from "features/TodolistsList/todolists.reducer";
 import { tasksThunks } from "features/TodolistsList/tasks.reducer";
-import { Grid, Paper } from "@mui/material";
+import { Grid, Paper, Skeleton } from "@mui/material";
 import { AddItemForm } from "common/components";
 import { Todolist } from "./Todolist/Todolist";
 import { Navigate } from "react-router-dom";
@@ -11,11 +15,13 @@ import { selectIsLoggedIn } from "features/auth/model/auth.selectors";
 import { selectTasks } from "features/TodolistsList/tasks.selectors";
 import { selectTodolists } from "features/TodolistsList/todolists.selectors";
 import { TaskStatuses } from "common/enums";
+import { selectIsInitialized } from "app/app.selectors";
 
 export const TodolistsList = () => {
   const todolists = useSelector(selectTodolists);
   const tasks = useSelector(selectTasks);
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isInitialized = useSelector(selectIsInitialized);
 
   const {
     removeTodolist: removeTodolistThunk,
@@ -24,7 +30,11 @@ export const TodolistsList = () => {
     changeTodolistTitle: changeTodolistTitleThunk,
   } = useActions(todolistsThunks);
 
-  const { addTask: addTaskThunk, removeTask: removeTaskThunk, updateTask } = useActions(tasksThunks);
+  const {
+    addTask: addTaskThunk,
+    removeTask: removeTaskThunk,
+    updateTask,
+  } = useActions(tasksThunks);
   const { changeTodolistFilter } = useActions(todolistsActions);
 
   useEffect(() => {
@@ -42,15 +52,26 @@ export const TodolistsList = () => {
     addTaskThunk({ title, todolistId });
   }, []);
 
-  const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
+  const changeStatus = useCallback(function (
+    taskId: string,
+    status: TaskStatuses,
+    todolistId: string
+  ) {
     updateTask({ taskId, domainModel: { status }, todolistId });
   }, []);
 
-  const changeTaskTitle = useCallback(function (taskId: string, title: string, todolistId: string) {
+  const changeTaskTitle = useCallback(function (
+    taskId: string,
+    title: string,
+    todolistId: string
+  ) {
     updateTask({ taskId, domainModel: { title }, todolistId });
   }, []);
 
-  const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
+  const changeFilter = useCallback(function (
+    filter: FilterValuesType,
+    id: string
+  ) {
     changeTodolistFilter({ id, filter });
   }, []);
 
@@ -78,22 +99,25 @@ export const TodolistsList = () => {
       <Grid container spacing={3}>
         {todolists.map((tl) => {
           let allTodolistTasks = tasks[tl.id];
-
           return (
             <Grid item key={tl.id}>
-              <Paper style={{ padding: "10px" }}>
-                <Todolist
-                  todolist={tl}
-                  tasks={allTodolistTasks}
-                  removeTask={removeTask}
-                  changeFilter={changeFilter}
-                  addTask={addTask}
-                  changeTaskStatus={changeStatus}
-                  removeTodolist={removeTodolist}
-                  changeTaskTitle={changeTaskTitle}
-                  changeTodolistTitle={changeTodolistTitle}
-                />
-              </Paper>
+              {isInitialized ? (
+                <Paper style={{ padding: "10px" }}>
+                  <Todolist
+                    todolist={tl}
+                    tasks={allTodolistTasks}
+                    removeTask={removeTask}
+                    changeFilter={changeFilter}
+                    addTask={addTask}
+                    changeTaskStatus={changeStatus}
+                    removeTodolist={removeTodolist}
+                    changeTaskTitle={changeTaskTitle}
+                    changeTodolistTitle={changeTodolistTitle}
+                  />
+                </Paper>
+              ) : (
+                <Skeleton variant="rectangular" width="100%" height={118} />
+              )}
             </Grid>
           );
         })}
